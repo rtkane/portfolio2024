c59b365598d0e27f445f8b8c227a66d7e48187d8
Ryan Kane <rtkane@byu.edu>
2025-01-27 11:33:52 -0700

    fix lint issues

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header";
 import Profile from "./components/profile/Profile";
 import Projects from "./components/projects/Projects";
